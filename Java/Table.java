bce67e4a7dc997b8810f40cbfbe43f1f88e093f2
dempseyb <ben.dempsey@unbc.ca>
2025-03-08 11:48:01 -0800

    Read in file to table done

--- Java/Table.java
+++ Java/Table.java
@@ -1,20 +1,57 @@
 package Java;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Table {
     private Scanner sc;
 
-    private int[][] table;
+    private Transaction[] table;
     private int TiD;
-    private int[] items;
-
-
+    private ArrayList<Integer> itemset; //varying sizes
 
     private void makeTable(String filename) {
         // Read the file and create the table
-        sc = new Scanner(filename);
-        while(sc.hasNextLine()) {
+        File file = new File("Data\\" + filename);
+        itemset = new ArrayList<Integer>();
+        try {
+            sc = new Scanner(file);
 
+            table = new Transaction[Integer.parseInt(sc.next())];// First line of the file is the number of transactions
+            int lineIndex = 0;
+            // sc.nextLine(); // Skip the rest of the first line
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine().trim();
+                String[] tokens = line.split("\\s+");
+                if (tokens.length < 2) {
+                    continue; // Skip lines that do not have at least TiD and one item
+                }
+                TiD = Integer.parseInt(tokens[0]);
+                for(int i = 2; i < tokens.length; i++) {//igore TiD and number of items
+                    itemset.add(Integer.parseInt(tokens[i]));
+                }
+                table[lineIndex++] = new Transaction(TiD, itemset);
+                itemset = new ArrayList<Integer>(); //Making new arraylist each time feels gross :(
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+
+    }
+
+    public String toString() { //PRINTING IS VERRRRRY SLOW
+        String tableString = "";
+        for (int i = 0; i < table.length; i++) {
+            tableString += table[i] + "\n";
+        }
+        return tableString;
+    }
+
+    public static void main(String[] args) {
+        Table table = new Table();
+        table.makeTable("connect.txt");
+        System.out.println(table);
     }
 }
---
+++ Java/Transaction.java
@@ -0,0 +1,24 @@
+package Java;
+
+import java.util.ArrayList;
+
+public class Transaction {
+    private int TiD;
+    private ArrayList<Integer> itemset;
+
+    public Transaction(int TiD, ArrayList<Integer> itemset) {
+        this.TiD = TiD;
+        this.itemset = itemset;
+    }
+
+    public int getTiD() {
+        return TiD;
+    }
+
+    public ArrayList<Integer> getItemset() {
+        return itemset;
+    }
+    public String toString() {
+        return TiD + " " + itemset;
+    }
+}

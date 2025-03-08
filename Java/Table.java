ed1a035c572aac39c2fb800b44d4c778783ea10e
dempseyb <ben.dempsey@unbc.ca>
2025-03-08 10:53:02 -0800

    Added base files

---
+++ Java/Table.java
@@ -0,0 +1,20 @@
+package Java;
+import java.util.Scanner;
+
+public class Table {
+    private Scanner sc;
+
+    private int[][] table;
+    private int TiD;
+    private int[] items;
+
+
+
+    private void makeTable(String filename) {
+        // Read the file and create the table
+        sc = new Scanner(filename);
+        while(sc.hasNextLine()) {
+
+        }
+    }
+}

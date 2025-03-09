9630d85b5320d1d56090c50c97c643ddede7cf54
Bdempsey03 <bdempsey@unbc.ca>
2025-03-08 18:43:48 -0800

    Made base classes

--- code/Transaction.java
+++ code/Transaction.java
@@ -1,4 +1,4 @@
-package Java;
+package code;
 
 import java.util.ArrayList;
 
---
+++ code/Tree.java
@@ -0,0 +1,9 @@
+package code;
+
+import java.util.ArrayList;
+
+public class Tree{
+    private TreeNode root;
+    private ArrayList<TreeNode> nodesInTree; //bucket of all nodes in the tree
+    private ArrayList<EntryTuple> entriesInTree; //This is the table associated with a tree
+}

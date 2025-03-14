1cab571d101b911c15818a7537c64e0e190e7251
dempseyb <ben.dempsey@unbc.ca>
2025-03-13 23:29:59 -0700

    Added a shit ton of comments

--- code/Tree.java
+++ code/Tree.java
@@ -11,40 +11,47 @@ public class Tree {
     private TreeNode root;
     private ArrayList<TreeNode> nodesInTree; // bucket of all nodes in the tree
     private ArrayList<EntryTuple> treeTable; // This is the table associated with a tree
-    private ArrayList<TreeNode> projectedTree;
-    private ArrayList<EntryTuple> projectedTable;
-    private int minsup;
+    private ArrayList<TreeNode> projectedTree; // Stores a projected tree's "bucket" of nodes
+    private ArrayList<EntryTuple> projectedTable; // a proejcted tree's table
+    private int minsup; //set by user
 
-    private ArrayList<int[]> frequentItemsets = new ArrayList<int[]>();
+    private ArrayList<int[]> frequentItemsets = new ArrayList<int[]>(); //this is constantly added to
 
     public Tree(int minsup) {
+        //init variables
         root = new TreeNode(true);
-        treeTable = new ArrayList<EntryTuple>();
+        treeTable = new ArrayList<EntryTuple>(); 
         nodesInTree = new ArrayList<TreeNode>();
-        nodesInTree.add(root);
+        nodesInTree.add(root);//add root to tree's nodes
         this.minsup = minsup;
     }
 
+    /*
+     * This method finds all itemsets of size 1 and their frequencies.
+     * The singletons are added to treeTable which is the table for the global tree.
+     * The table is not sorted and singletons with support < minsup ARE still included (dealt with later)
+     */
     public void findSingletons(String filename) {
         Scanner sc;
         File file = new File("Data\\" + filename);
         try {
             sc = new Scanner(file);
-
             treeTable = new ArrayList<EntryTuple>();// First line of the file is the number of transactions
-            // sc.nextLine(); // Skip the rest of the first line
-            while (sc.hasNextLine()) {
+            while (sc.hasNextLine()) {//continue until end of file
                 String line = sc.nextLine().trim();
-                String[] tokens = line.split("\\s+");
+                String[] tokens = line.split("\\s+");//split transaction into individual tokens
                 if (tokens.length < 2) {
                     continue; // Skip lines that do not have at least TiD and one item
                 }
 
-                for (int i = 2; i < tokens.length; i++) {// igore TiD and number of items
+                for (int i = 2; i < tokens.length; i++) {// ignore TiD and number of items
 
+                    
                     if (!tableContains(Integer.parseInt(tokens[i]))) {
+                        //if the tree table does not already have this token then we need to add it
                         treeTable.add(new EntryTuple(Integer.parseInt(tokens[i]), 1));
                     } else {
+                        //if the Tree Table already has this token in it then we increase the support rather than adding a new token
                         incrementSupport(Integer.parseInt(tokens[i]));
                     }
                 }
@@ -55,6 +62,9 @@ public class Tree {
 
     }
 
+    /*
+     * Helper method that just adds 1 to the support of an item
+     */
     private void incrementSupport(int item) {
         for (int i = 0; i < treeTable.size(); i++) {
             if (treeTable.get(i).getItem() == item)
@@ -62,6 +72,10 @@ public class Tree {
         }
     }
 
+    /*
+     * Helper method that checks every tuple in the table to see if it is contained in the table
+     * @returns boolean "does the table contain item?"
+     */
     private boolean tableContains(int item) {
         for (EntryTuple e : treeTable) {
             if (e.getItem() == item)
@@ -70,6 +84,9 @@ public class Tree {
         return false;
     }
 
+    /*
+     * Sorts list of singletons and removes ones with support less than the minsup
+     */
     public void sortTable() {
         treeTable.sort(Comparator.comparingInt(EntryTuple::getSupport).reversed());
         while(treeTable.get(treeTable.size()-1).getSupport() < minsup){
@@ -77,22 +94,27 @@ public class Tree {
         }
     }
 
-    public void makeGlobalTree(String filename) {
 
-        /*
-         * For all transactions in parsedTable insert all the items
-         * 
-         * Start at root and check if any of root.getChildren() contain an item in the
-         * transaction.
-         * If yes -> increment support and go down that path.
-         * If no -> add a new branch with the remainder of the transaction.
-         * 
-         * Go back to root and repeat for next transaction.
-         */
+    /**
+     * Parses a file and constructs a global m-way FP tree.
+     * 
+     * For every transaction t in the transaction DB we sort the transaction in order of most frequent singletons,
+     * next we construct the tree from the root downwards. 
+     * First, we add the first element of t1 as the child of the root,
+     * next we add the second element of t1 as the child of the first,
+     * repeat until t1 is added.
+     * 
+     * Next, if t2's first element is the same as t1 we follow down that branch increasing the support
+     * until we find an element that was NOT in t1. Now we split into a new branch inserting t2. Repeat for 
+     * t3, and t4, ...
+     * 
+     * @param filename
+     */
+    public void makeGlobalTree(String filename) {
 
         // Start at root:
         Scanner sc;
-        TreeNode currentNode = root; // item in root is NULL
+        TreeNode currentNode = root; // item in root is -1
 
         // Read the file and create the table
         File file = new File("Data\\" + filename);
@@ -175,8 +197,7 @@ public class Tree {
                                     t.getItemset().get(0));
                             // POINTER ADDED
                             t.getItemset().remove(0);// shrink itemset (TODO: REVISIT BECAUSE SLOW)
-                            // if(!currentNode.isRoot)
-                            // currentNode.entry.setSupport(currentNode.entry.getSupport()+1);
+
                             nodesInTree.add(currentNode.getChildren().get(currentNode.getChildren().size() - 1));
                             currentNode = currentNode.getChildren().get(currentNode.getChildren().size() - 1);
                         }
@@ -188,9 +209,13 @@ public class Tree {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        // System.out.println(this.toStringFreq(1));
     }
 
+    /*
+     * Helper method that adds the appropriate pointer to a node in a tree.
+     * 
+     * INNEFFICIENT :(
+     */
     private void addPointer(TreeNode newNode, int item) {
         EntryTuple ptr;
         EntryTuple next;
@@ -205,6 +230,11 @@ public class Tree {
         ptr.setNext(newNode.getEntryTuple());
 
     }
+
+    /*
+     * This method SHOULD iterate through all subtrees and subtrees of subtrees but it current just creates
+     * the x-projected trees for the singletons in the DB
+     */
     public void projectSubtrees() {
         for (int i = treeTable.size()-1; i > 0; i--) {
             frequentItemsets.addAll(buildProjectedTree(treeTable, nodesInTree, treeTable.get(i).getItem()));
@@ -212,6 +242,9 @@ public class Tree {
 
     }
 
+    /*
+     * THis method makes the projected subtrees from a tree (arraylist of nodes) and a tree table with a value to project (val)
+     */
     public ArrayList<int[]> buildProjectedTree(ArrayList<EntryTuple> prevTable, ArrayList<TreeNode> prevTree, int val) {
 
         ArrayList<int[]> frequentItemsets = new ArrayList<int[]>();
@@ -227,33 +260,28 @@ public class Tree {
         // Add all nodes in the path to the root to the projected tree
 
 
+        TreeNode currentNode = firstNode;//rename to a more useful name
 
-        // GOOD UNTIL HERE
+        /*TODO:
+         * This assumption is not true if the projected tree has branches that join higher up. Need to revisit!
+         */
+        int sup = firstNode.getEntryTuple().getSupport();//the support of all nodes between this node and the root are the same as this one 
+        start=start.getNext();
 
-        TreeNode currentNode = firstNode;
-        // System.out.println(currentNode+"!!!");
-        int sup = firstNode.getEntryTuple().getSupport();
-
-        while (start != null) {
-            // if(start!=null)
-
-            // currentNode = getNodeOfEntry(prevTree, start.getNext());
-
-            // System.out.println("This is the current node and the next one in the list:"+ currentNode + start);
-
+        while (start != null) {//This loop goes "laterally" from one node to its NEXT POINTER. 
             
-            while (currentNode != null) {
-                if (currentNode.getParent() == null && noRoot) {
+            while (currentNode != null) {//THis loops traverses UP a tree branch and stops when we reach the root
+                if (currentNode.getParent() == null && noRoot) {//If this projected tree doesnt have a root and we have reached the top we add a root
                     projectedRoot = new TreeNode(true);
                     projectedTree.add(projectedRoot);
                     noRoot = false;
                     break;
                 } else {
-                    if (currentNode.getParent() == null) {
+                    if (currentNode.getParent() == null) {//if there is a root and weve reached the parent being NULL then we have reached the top
                         break;
                     }
                 }
-                if (!currentNode.getParent().isRoot) {
+                if (!currentNode.getParent().isRoot) {//if we arent in the root then we add the current node and continue upwards
                     projectedTree.add(new TreeNode(new EntryTuple(currentNode.getParent().getEntryTuple().getItem(),
                             sup), currentNode.getParent(),
                             new ArrayList<TreeNode>()));
@@ -269,31 +297,26 @@ public class Tree {
             }
             start = start.getNext();//Move laterally
             if(start!=null)
-                currentNode = getNodeOfEntry(prevTree, start.getNext());
-        // if(start!=null)
-        //     currentNode = getNodeOfEntry(prevTree, start);
-            // System.out.println(start + "!!");
+                currentNode = getNodeOfEntry(prevTree, start);
+
         }
-        // System.out.println(projectedTable + ", "+val);
-        for(EntryTuple e : projectedTable){
-            if(e.getItem()!=-1){//skip root
-                // System.out.println(val+","+e.getItem() + "!");
-                frequentItemsets.add(new int[]{val, e.getItem()});
+
+        //THIS IS PROBABLY WHERE THE RECURIVE CALL SHOULD BE(?)
+
+        for(EntryTuple e : projectedTable){ //add discovered frequent items to set
+            if(e.getItem()!=-1){//dont add the root in a set
+                frequentItemsets.add(new int[]{val, e.getItem()});//add the prefix (the thing we projected) + every frequent item in the table for the projected tree
             }
         }
         //TODO: Filter out all entries with sup < minsup
 
-        
-
-        //base 
-        // buildProjectedTree(projectedTable.get(projectedTable.size()-1).getItem()); //Recursion :(
-        // projectedTable.remove(projectedTable.size()-1);
-
-        
         return frequentItemsets;
 
     }
 
+    /*
+     * Helper method to find the node in the tree
+     */
     public TreeNode getNodeOfEntry(ArrayList<TreeNode> nodesInTree, EntryTuple e) {
         for (TreeNode n : nodesInTree) {
             if (n.getEntryTuple().equals(e)){
@@ -301,7 +324,7 @@ public class Tree {
             }
         }
 
-        // System.out.println("Cannot find node with entry: " + e +"\n");
+        System.out.println("Cannot find node with entry: " + e +"\n");
         // new Exception().printStackTrace();
         return null;
     }
@@ -311,6 +334,7 @@ public class Tree {
             if (e.getItem() == val)
                 return e;
         }
+        System.out.println("Didnt find singleton with value of "+val);
         return null;
     }
 

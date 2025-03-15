922f35448dfe45bd6e1dc6c1554110b751ddb59a
katieKilburn <kilburn@unbc.ca>
2025-03-14 22:59:33 -0700

    updated mining result output

--- code/WriteToFile.java
+++ code/WriteToFile.java
@@ -11,10 +11,11 @@ public class WriteToFile{
             writer = new FileWriter("MiningResults_"+dataset);
             writer.write("FPs = "+frequentTuples.size()+"\n");
             for (PatternTuple pattern : frequentTuples){
-                for(int i = 0; i < pattern.getFreqPattern().length; i++){
+                for(int i = 0; i < pattern.getFreqPattern().length - 1; i++){
                     writer.write(pattern.getFreqPattern()[i]+", ");
                 }
-                writer.write(": "+pattern.getSupport()+"\n");
+
+                writer.write(pattern.getFreqPattern()[pattern.getFreqPattern().length - 1]+" : "+pattern.getSupport()+"\n");
             }
             writer.close();
         }catch (IOException e) {
